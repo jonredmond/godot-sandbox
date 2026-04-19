789a49fdf458020f773ce3f19d94950abaa61f6c
Jonathan Redmond <jonredmond94@gmail.com>
2026-04-19 12:34:20 +0100

    Add bouncing ball with walled field

---
+++ projects/2d_pong/ball.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+@export var speed: float = 400.0
+
+func _ready() -> void:
+	_launch()
+	
+func _launch() -> void:
+	# Pick a random angle within 45 degrees of horizontal, on either side.
+	var angle: float = randf_range(-PI / 4.0, PI / 4.0)
+	if randf() < 0.5:
+		angle += PI
+	
+	velocity = Vector2(cos(angle), sin(angle)) * speed
+
+func _physics_process(delta: float) -> void:
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
+		
